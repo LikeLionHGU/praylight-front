f70f1b54afdfa954a2fb6fd5dff88e07313bdcb2
Hyelim Choi <HyelimChoi01@gmail.com>
2023-11-02 04:37:43 +0900

    feat: add google login

--- src/auth/GoogleButton.jsx
+++ src/auth/GoogleButton.jsx
@@ -1,30 +1,32 @@
 // eslint-disable-next-line no-unused-vars
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
-import jwtDecode from 'jwt-decode';
-import { useHistory } from 'react-router-dom';
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import jwtDecode from "jwt-decode";
+import { useHistory } from "react-router-dom";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { IsLoginState } from "../store/atom";
 // import { useRecoilState, useSetRecoilState } from 'recoil';
 // import { IsLoginState, UserEmail } from '../store/atom';
 
 export default function GoogleButton() {
   // eslint-disable-next-line no-unused-vars
-  //   const [Login, setLogin] = useRecoilState(IsLoginState);
-  //   const setUserEmail = useSetRecoilState(UserEmail);
+  const setLogin = useSetRecoilState(IsLoginState);
+  // const setUserEmail = useSetRecoilState(setUserEmail);
 
   let history = useHistory();
 
-  const onSuccess = async credentialResponse => {
+  const onSuccess = async (credentialResponse) => {
     const decodedToken = jwtDecode(credentialResponse.credential);
     console.log(decodedToken);
     // setUserEmail(decodedToken.email);
-    // setLogin(true);
-    history.push('/addInfo');
+    setLogin(true);
+    history.push("/addInfo");
   };
 
   return (
     <>
       <GoogleLogin
-        onSuccess={credentialResponse => onSuccess(credentialResponse)}
-        onFailure={error => console.log(error)}
+        onSuccess={(credentialResponse) => onSuccess(credentialResponse)}
+        onFailure={(error) => console.log(error)}
         useOneTap
       />
     </>
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,3 +1,22 @@
+import { FaPrayingHands } from "react-icons/fa";
+import styled from "styled-components";
+import GoogleButton from "../auth/GoogleButton";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 100vh;
+`;
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <Container>
+        <FaPrayingHands size={"30px"} />
+        <GoogleButton />
+      </Container>
+    </>
+  );
 }
