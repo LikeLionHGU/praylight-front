34d84fe7392563af64a3f1cef6c6b88e1db6c44a
Hyelim Choi <HyelimChoi01@gmail.com>
2023-11-02 04:41:10 +0900

    feat: praise card

--- src/components/PraiseCard.tsx
+++ src/components/PraiseCard.tsx
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 import theme from "../theme";
-import { AiTwotoneStar } from "react-icons/ai";
+import { useState } from "react";
 
+import prayOff from "../imgs/prayOff.png";
+import prayOn from "../imgs/prayOn.png";
+import editIcon from "../imgs/editIcon.png";
+import deleteIcon from "../imgs/deleteIcon.png";
+
+const mine = {
+  isMine: true,
+};
 interface Pray {
   pid: number;
   name: string;
@@ -10,13 +18,13 @@ interface Pray {
 }
 
 const Container = styled.div`
-  background-color: ${theme.palette.mono.gray4};
+  background-color: ${theme.palette.color.gray7};
   padding: 20px;
   border-radius: 8px;
   margin-bottom: 10px;
 `;
 
-const Tops = styled.div`
+const Rows = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -35,22 +43,49 @@ const Btn = styled.div`
   display: flex;
   justify-content: flex-end;
   align-items: center;
-  gap: 5px;
+  gap: 10px;
   font-size: 12px;
 `;
 
+const Icon = styled.img`
+  width: 15px;
+`;
+
+const Praied = styled.img`
+  width: 120px;
+`;
+
 export default function PraiseCard({ pray }: { pray: Pray }) {
+  const [prayed, setPrayed] = useState(false);
+
+  const togglePrayed = () => {
+    setPrayed(!prayed);
+  };
   return (
     <>
       <Container>
-        <Tops>
+        <Rows>
           <Top>{pray.name}</Top>
           <Top>D-{pray.Dday}</Top>
-        </Tops>
+        </Rows>
         <Body>{pray.praiseContent}</Body>
-        <Btn>
-          <AiTwotoneStar /> 함께 기도해주세요
-        </Btn>
+        <Rows>
+          {mine.isMine ? (
+            <Btn>
+              <Icon src={editIcon} />
+              <Icon src={deleteIcon} />
+            </Btn>
+          ) : (
+            <div />
+          )}
+          <Btn>
+            {prayed ? (
+              <Praied src={prayOn} onClick={togglePrayed} />
+            ) : (
+              <Praied src={prayOff} onClick={togglePrayed} />
+            )}
+          </Btn>
+        </Rows>
       </Container>
     </>
   );
