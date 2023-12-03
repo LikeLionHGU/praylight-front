648b83bf2ed60cd02472bd699d4cec305ae6c667
Hyelim Choi <HyelimChoi01@gmail.com>
2023-12-03 23:44:16 +0900

    feat: separate as new component

---
+++ src/components/RoomInfo.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import theme from "../theme";
+import { Iroom } from "../types/type";
+import AddPrayDialog from "./add-pray-dialog";
+
+const Top = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: space-between;
+  margin-bottom: 10px;
+`;
+
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const Ppl = styled.div`
+  color: ${theme.palette.color.gray4};
+  font-size: 12px;
+  padding: 5px 0px;
+`;
+
+export default function RoomInfo({ roomInfo }: { roomInfo: Iroom }) {
+  return (
+    <>
+      <Top>
+        <div>
+          <Title> {roomInfo?.title} </Title>
+          <Ppl> {roomInfo?.roomPpl}명 참여 </Ppl>
+        </div>
+        <AddPrayDialog currentRoom="AAA" />
+      </Top>
+    </>
+  );
+}
