2995c4df9974f208f13334829f42c03a8610218f
Hyelim Choi <HyelimChoi01@gmail.com>
2023-12-03 23:45:51 +0900

    docs: new types
    - Iprayer
    - Iuser

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,6 +1,23 @@
 export type Iroom = {
-  rId: number;
+  id: number;
   title: string;
   roomPpl: number;
   lastActivityDate: string;
+  lastClickedToday: boolean;
+};
+
+export type Iprayer = {
+  id: number;
+  name: string;
+  pray: string;
+  prayDate: string;
+  prayCount: number;
+  roomId: number;
+};
+
+export type Iuser = {
+  id: number;
+  uid: string;
+  name: string;
+  email: string;
 };
