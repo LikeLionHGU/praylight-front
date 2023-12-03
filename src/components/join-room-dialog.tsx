06dc07253dd0e85164b0ae221a996b6d38fe5fbd
Hyelim Choi <HyelimChoi01@gmail.com>
2023-12-04 04:01:46 +0900

    feat: add api

--- src/components/join-room-dialog.tsx
+++ src/components/join-room-dialog.tsx
@@ -5,6 +5,9 @@ import DialogTitle from "@mui/material/DialogTitle";
 import styled, { createGlobalStyle } from "styled-components";
 import { Form, Input, Modal, Button } from "antd";
 import theme from "../theme";
+import { useRecoilValue } from "recoil";
+import { UserIdState } from "../store/atom";
+import axiosInstance from "../axios";
 
 const GlobalStyle = createGlobalStyle`
    .custom-modal .ant-modal-content {
@@ -61,35 +64,16 @@ const Btn = styled.div`
   border-radius: 18px;
 `;
 
-const SmallBtn = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 70px;
-  height: 35px;
-  border: 1px solid white;
-  border-radius: 18px;
-  color: white;
-  margin-bottom: 0px;
-`;
-
-const Rows = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  gap: 10px;
-  margin-top: 10px;
-`;
-
 const NoFoundRoom = styled.div`
   color: ${theme.palette.color.gray3};
   font-size: 12px;
+  margin-top: 5px;
 `;
 
 export default function JoinRoomDialog() {
+  const userId = useRecoilValue(UserIdState);
   const [open, setOpen] = React.useState(false);
   const [newModalOpen, setNewModalOpen] = React.useState(false);
-
   const [find, setFind] = React.useState(true);
   const [room, setRoom] = React.useState("");
 
@@ -99,19 +83,24 @@ export default function JoinRoomDialog() {
 
   const onSubmit = (values: any) => {
     console.log(values);
-    setOpen(false);
-    setNewModalOpen(true);
+
+    const response = axiosInstance
+      .post(`/rooms/user/${userId}/code/${values.code}`)
+      .then((response) => {
+        setOpen(false);
+        setNewModalOpen(true);
+        setRoom(response.data.title);
+      })
+      .catch((error) => {
+        setFind(false);
+      });
+
+    return response;
   };
 
   const handleNewRoom = () => {
     setNewModalOpen(false);
     console.log(room);
-  };
-
-  const findRoom = () => {
-    console.log("findRoom");
-    setFind(false);
-    setRoom("김호준 교수님 PRS");
   };
 
   return (
@@ -132,17 +121,14 @@ export default function JoinRoomDialog() {
           <DialogContentText style={{ color: "white", padding: "5px 0px" }}>
             참여하실 기도방의 코드를 입력해주세요.
           </DialogContentText>
-          <Rows>
-            <Form.Item name="pray_content" style={{ marginBottom: "0px" }}>
-              <Input
-                placeholder="기도방 코드를 입력하세요."
-                style={{
-                  backgroundColor: theme.palette.color.gray1,
-                }}
-              />
-            </Form.Item>
-            <SmallBtn onClick={findRoom}> 찾기 </SmallBtn>
-          </Rows>
+          <Form.Item name="code" style={{ marginBottom: "0px" }}>
+            <Input
+              placeholder="기도방 코드를 입력하세요."
+              style={{
+                backgroundColor: theme.palette.color.gray1,
+              }}
+            />
+          </Form.Item>
           {find ? (
             <div />
           ) : (
@@ -217,7 +203,7 @@ export default function JoinRoomDialog() {
               marginTop: "15px",
             }}
           >
-            이 기도방에 함께 참여하시겠습니까?
+            지금부터 {room} 과 함께 기도를 나누실 수 있습니다!
           </DialogContentText>
         </DialogContent>
       </Modal>
